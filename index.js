f80a7033423c63a53f9f41542aec156ae176f35b
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2021-11-10 22:07:44 +0600

    add

--- index.js
+++ index.js
@@ -14,19 +14,35 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // console.log(uri)
 
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run(){
     try{
-         await client.connect();
-        console.log('database connected successfully')
+        await client.connect();
+        console.log('connected to database')
+        const database = client.db('tourismHotel');
+        const productsCollection = database.collection('products')
 
-        const database = client.db('Perfume_Pride');
-        const productsCollection = database.collection('products');
+        // //user collection
+        // const usersCollection = database.collection('users');
 
-        //user collection
-        const usersCollection = database.collection('users');
+// GET API
+        app.get('/products', async(req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
 
+
+        //POST API
+        // app.post('/products', async (req, res) => {
+        //     const products = req.body;
+        //     console.log('Hit the post api', service);
+        //     const result = await servicesCollection.insertOne(service);
+        //     console.log(result);
+        //     res.json(result);
+        // });
     }
 
 
@@ -40,14 +56,7 @@ async function run(){
     }
 }
 
-
-
-
-
-
-
-
-
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello Niche website')
@@ -56,3 +65,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`listening at ${port}`)
 })
+
+
+
+
+
+
+

75a1fbf0f10060c50738bd902d1560c1dccd3e09
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2021-11-13 00:35:36 +0600

    add

--- index.js
+++ index.js
@@ -100,20 +100,20 @@ async function run(){
 // admin
         app.put('/users/admin', async(req, res) => {
             const user = req.body;
-            const requester = req.decodeEmail;
-            if(requester){
-                const requesterAccount = await usersCollection.findOne({email: requester});
-                if(requesterAccount.role === 'admin'){
+            // const requester = req.decodeEmail;
+            // if(requester){
+            //     const requesterAccount = await usersCollection.findOne({email: requester});
+            //     if(requesterAccount.role === 'admin'){
                     const filter = { email: user.email};
                     const updateDoc = { $set: { role: 'admin' } };
                     const result = await usersCollection.updateOne(filter, updateDoc);
                     res.json(result);
-                }
-            }
+            //     }
+            // }
 
-            else{
-                res.status(403).json({ message: 'you do not have access to make admin' })
-            }
+            // else{
+            //     res.status(403).json({ message: 'you do not have access to make admin' })
+            // }
         })
 
 
